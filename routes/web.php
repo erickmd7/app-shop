2ab2518dd541a82c740ed040f352a3ca4b9d92bc
Erick Morales <l17550857@chihuahua2.tecnm.mx>
2023-01-03 14:45:26 -0600

    Base Proyecto

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\HomeController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [TestController::class,'welcome']);
+
+Route::get('/prueba', function(){
+    return 'Hola soy una ruta de prueba';
+});
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
